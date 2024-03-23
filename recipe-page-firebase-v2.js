c7a91ed8990e5fc4bbeee27ef6c17d934d144759
simonrayray <79811918+simonrayray@users.noreply.github.com>
2024-03-23 23:29:22 +0100

    Update recipe-page-firebase-v2.js

--- recipe-page-firebase-v2.js
+++ recipe-page-firebase-v2.js
@@ -36,15 +36,6 @@ export async function fetchRecipeData(recipeId) {
     }
 }
 
-// Global toggle state for including starter in calculations
-let includeStarterFlour = false;
-
-// Function to toggle the inclusion of starter flour in calculations
-function toggleIncludeStarterFlour() {
-    includeStarterFlour = !includeStarterFlour;
-    updatePageWithRecipeData(recipeData);
-}
-
 // Utility Functions
 function hideLoader() {
     document.querySelector('.loader-wrapper').style.display = 'none';
@@ -54,19 +45,14 @@ function getUnit(format) {
     return format === 'grams' ? 'g' : format === 'ounces' ? 'oz' : 'Unknown unit';
 }
 
-// Function to update the recipe image or hide it if not available
+// Function to update the recipe image
 function updateImageSrc(recipeData) {
-    const imageUrl = recipeData.image_url;
-    const thumbnailImageElement = document.querySelector('.recipe-image');
-    const imageWrapper = thumbnailImageElement ? thumbnailImageElement.parentElement : null;
 
-    if (thumbnailImageElement && imageUrl) {
+    const imageUrl = recipeData.image_url
+
+    const thumbnailImageElement = document.querySelector('.recipe-image');
+    if (thumbnailImageElement && recipeData.image_url) {
         thumbnailImageElement.src = imageUrl;
-        // Ensure the wrapper is visible in case it was previously hidden
-        if (imageWrapper) imageWrapper.classList.remove('is-hidden');
-    } else {
-        // Hide the wrapper if no image URL is provided
-        if (imageWrapper) imageWrapper.classList.add('is-hidden');
     }
 }
 
@@ -82,61 +68,17 @@ function formatDate(firestoreTimestamp) {
 }
 
 function calculateTotalWeight(ingredients) {
-    // Ensure ingredients is an array and not undefined or null
-    if (!Array.isArray(ingredients) || ingredients.length === 0) {
-        return 0; // Return 0 if ingredients is not an array or is empty
-    }
-
-    return ingredients.reduce((total, ingredient) => {
-        // Use display_weight for "Starter" ingredients if it's present and hydration is undefined
-        if (ingredient.type === "Starter" && ingredient.hydration === undefined) {
-            return total + (Number(ingredient.displayWeight) || 0);
-        }
-        // Otherwise, use weight
-        return total + (Number(ingredient.weight) || 0);
-    }, 0);
+    return ingredients.reduce((total, ingredient) => total + ingredient.weight, 0);
 }
 
-
-// Function to calculate total fluid weight, including starter water
-function calculateTotalFluidWeight(ingredients) {
-    if (!Array.isArray(ingredients) || ingredients.length === 0) {
-        return 0;
-    }
-
-    return ingredients.reduce((total, ingredient) => {
-        if (ingredient.type === 'Fluid') {
-            return total + (Number(ingredient.weight) || 0);
-        } else if (ingredient.starter && ingredient.type === 'Starter' && ingredient.hydration === undefined) {
-            return total + (Number(ingredient.displayWeight) || 0);
-        }
-        return total;
-    }, 0);
+function calculateFlourWeight(ingredients) {
+    return ingredients.filter(ingredient => ingredient.type === "Flour").reduce((total, ingredient) => total + ingredient.weight, 0);
 }
 
-// Function to calculate total flour weight, including starter flour
-function calculateTotalFlourWeight(ingredients) {
-    if (!Array.isArray(ingredients) || ingredients.length === 0) {
-        return 0;
-    }
-
-    return ingredients.reduce((total, ingredient) => {
-        if (ingredient.type === 'Flour') {
-            return total + (Number(ingredient.weight) || 0);
-        } else if (ingredient.starter && ingredient.type === 'Starter' && ingredient.hydration === undefined) {
-            return total + (Number(ingredient.displayWeight) || 0);
-        }
-        return total;
-    }, 0);
+function calculateHydration(flourWeight, ingredients) {
+    const waterWeight = ingredients.filter(ingredient => ingredient.type === "Fluid").reduce((total, ingredient) => total + ingredient.weight, 0);
+    return (waterWeight / flourWeight) * 100;
 }
-
-// Function to calculate hydration, including starter
-function calculateHydration(ingredients) {
-    const totalFlourWeight = calculateTotalFlourWeight(ingredients);
-    const totalWaterWeight = calculateTotalFluidWeight(ingredients);
-    return (totalWaterWeight / totalFlourWeight) * 100;
-}
-
 
 function toggleIngredientSectionsVisibility(prefermentIngredients, extraIngredients) {
     // Toggle preferment section visibility
@@ -189,44 +131,24 @@ function updateIngredientsList(selector, ingredients, totalFlourWeight) {
 
     list.innerHTML = ''; // Clear the list
     ingredients.forEach(ingredient => {
-        // Skip rendering starter ingredients in flour and fluid lists
-        if (ingredient.starter) {
-            return; // Skip this iteration if the ingredient is part of a starter
-        }
-
         const clone = document.importNode(template, true);
-
-        // Set name
+        const percent = totalFlourWeight ? (ingredient.weight / totalFlourWeight) * 100 : 0;
         clone.querySelector('[recipe="ingredient-name"]').textContent = ingredient.name;
+        clone.querySelector('[recipe="ingredient-weight"]').textContent = `${ingredient.weight}`;
 
-        // Set weight, adjusting for starter ingredients if needed
-        const weight = ingredient.type === "Starter" && ingredient.displayWeight ? ingredient.displayWeight : ingredient.weight;
-        clone.querySelector('[recipe="ingredient-weight"]').textContent = `${weight}`;
-
-        // Handling hydration percent visibility (if applicable to the list)
-        const hydrationPercentElement = clone.querySelector('[recipe="hydration-percent"]');
-        if (hydrationPercentElement) {
-            const hydrationPercentWrapper = hydrationPercentElement.parentElement;
-            if (ingredient.hydration !== undefined) {
-                hydrationPercentElement.textContent = `${ingredient.hydration}%`;
-                hydrationPercentWrapper.classList.remove('is-hidden');
-            } else {
-                hydrationPercentWrapper.classList.add('is-hidden');
-            }
-        }
-
-        // Setting percent value for ingredients except 'Extra'
-        if (ingredient.type !== 'Extra') {
+        // Conditionally hide the weight wrapper if weight is not set or 0
+        if (ingredient.type === 'Extra' && (!ingredient.weight || ingredient.weight === 0)) {
+            clone.querySelector('[recipe="extras-weight-wrapper"]').classList.add('is-hidden');
+        } else if (ingredient.type !== 'Extra') {
+            // Only set percent if ingredient is not 'Extra', checking the ingredient's type
             const percentElement = clone.querySelector('[recipe="ingredient-percent"]');
-            if (percentElement) {
-                const flourWeightForCalculation = totalFlourWeight;
-                const percent = flourWeightForCalculation ? (weight / flourWeightForCalculation) * 100 : 0;
+            if (percentElement && ingredient.type !== 'Extra' && totalFlourWeight) {
                 percentElement.textContent = `${percent.toFixed(1)}%`;
             }
         }
-
         list.appendChild(clone);
     });
+
 }
 
 // Main Update Function
@@ -243,19 +165,6 @@ function updatePageWithRecipeData(recipeData) {
     const unit = getUnit(recipeData.format);
     document.querySelectorAll('[recipe="format"]').forEach(element => element.textContent = unit);
 
-    // Hide steps header if no steps in list
-    const stepsSection = document.getElementById('steps');
-    if (recipeData.steps && recipeData.steps.length < 1) {
-        // If there are no steps, hide the steps section
-        if (stepsSection) {
-            stepsSection.classList.add('is-hidden');
-        }
-    } else {
-        // If there are steps, make sure the section is visible
-        if (stepsSection) {
-            stepsSection.classList.remove('is-hidden');
-        }
-    
     // Update recipe steps
     const stepsListElement = document.querySelector('ul[recipe="steps-list"]');
 
@@ -274,12 +183,12 @@ function updatePageWithRecipeData(recipeData) {
 
     // Calculate dough stats
     const totalDoughWeight = calculateTotalWeight(totalDoughIngredients);
-    const totalFlourWeight = calculateTotalFlourWeight(totalDoughIngredients);
+    const totalFlourWeight = calculateFlourWeight(totalDoughIngredients);
     const hydration = calculateHydration(totalFlourWeight, totalDoughIngredients);
 
     // Calculate preferment stats
     const totalPrefermentDoughWeight = calculateTotalWeight(prefermentIngredients);
-    const totalPrefermentFlourWeight = calculateTotalFlourWeight(prefermentIngredients);
+    const totalPrefermentFlourWeight = calculateFlourWeight(prefermentIngredients);
     const prefermentHydration = calculateHydration(totalPrefermentFlourWeight, prefermentIngredients);
 
     // Dough stats overview
@@ -325,7 +234,6 @@ function updatePageWithRecipeData(recipeData) {
     document.title = `${recipeData.title || "Recipe made with Homebaker"} | Made with Homebaker`;
 
     hideLoader();
-    }
 }
 
 // Function to render the recipe steps list
@@ -395,17 +303,13 @@ function renderStepsListItems(listElement, items, templateHTML, templateClass) {
 }
 
 
+
 async function main() {
     const recipeId = new URLSearchParams(window.location.search).get('id');
     const recipeData = await fetchRecipeData(recipeId);
     if (recipeData) {
-        console.log(recipeData.ingredients);
         updatePageWithRecipeData(recipeData);
     }
-
-    // Attach event listener to toggle
-    document.getElementById('starterToggle').addEventListener('change', toggleIncludeStarterFlour);
-
 }
 
 main();
